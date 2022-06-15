67b37fefa2046949ba62d11ea2ee495e7efae222
BenMueller1 <benjm2002@gmail.com>
2022-06-14 22:41:54 -0500

    init commit

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
+    ''' since this inherits from AbstractUser, it already has field for username, email, password, etc.'''
+    # TODO add a watchlist
     pass
+
+# TODO auction listing model
+
+# TODO bid model
+
+# TODO comment on auction listing model
