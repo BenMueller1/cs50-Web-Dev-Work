74bcacfaad05b548945e8626042ed901e8f361bc
BenMueller1 <benjm2002@gmail.com>
2022-06-15 07:40:50 -0500

    made Listing, Bid, Comment models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,14 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from datetime import datetime
 
 class User(AbstractUser):
     ''' since this inherits from AbstractUser, it already has field for username, email, password, etc.'''
     # TODO add a watchlist
     pass
 
-# TODO auction listing model
+class Listing(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    current_price = models.FloatField()
+    active = models.BooleanField()
+    image_url = models.URLField(default=None)  # should this be a FilePathField ?
+    category = models.CharField(max_length=30, default=None)
 
-# TODO bid model
 
-# TODO comment on auction listing model
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    amount = models.FloatField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    posted_datetime = models.DateTimeField(default=datetime.now())
